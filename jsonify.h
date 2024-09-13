05d38bab9a0c576a52e9766cf67a12986af53b55
Bjoern Boss Henrichsen <bjoernbossdev@gmail.com>
2024-09-14 00:45:17 +0200

    Add jsonify file and licensing comments and bsd-3 license

---
+++ jsonify.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright (c) 2024 Bjoern Boss Henrichsen */
+#pragma once
+
+#include "json-builder.h"
+#include "json-reader.h"
+#include "json-serialize.h"
+#include "json-deserialize.h"
+#include "json-value.h"
